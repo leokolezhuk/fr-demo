9c06111a861a91d49395411203c7cc62d3e0ea4c
QUANTIB-LEO\Leo Kolezhuk <l.kolezhuk@quantib.com>
2021-10-17 00:29:40 +0200

    Removed the regenerator import from index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import '@node_modules/core-js/stable';
-import '@node_modules/regenerator-runtime/runtime';
+// import '@node_modules/regenerator-runtime/runtime';
 import NHTSAStore from '@modules/nhtsa_store';
+
 window.x = new NHTSAStore();
+
+
 new Vue({
   el: '#app',
   render: h => h(App)
