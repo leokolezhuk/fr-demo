f9c054660ee39c4349d9ee23cad1a53f8ce22f52
QUANTIB-LEO\Leo Kolezhuk <l.kolezhuk@quantib.com>
2021-10-17 00:28:28 +0200

    Added Loader component

---
+++ src/js/components/Loader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="spinner-grow text-primary"
+       role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Loader",
+  }
+</script>
+
+<style scoped>
+
+</style>
