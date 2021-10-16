6f4cec33cee69688219f6dee526ecb66b86eb250
QUANTIB-LEO\Leo Kolezhuk <l.kolezhuk@quantib.com>
2021-10-17 00:28:46 +0200

    Addde the RadioButtonSelector component

---
+++ src/js/components/RadioButtonSelector.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="btn-group">
+    <template v-for="option in options">
+      <input type="radio" class="btn-check"
+             name="fuelTypeSelection"
+             :id="getOptionId(option)"
+             :value="getOptionId(option)"
+             v-model="selectedOptionId"
+             autocomplete="off">
+      <label class="btn btn-outline-primary"
+             :for="getOptionId(option)"
+             v-html="option.content"
+             :title="option.title">
+      </label>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RadioButtonSelector",
+    props: {
+      id: {
+        type: String,
+        required: true,
+      },
+      options: {
+        type: Array,
+        required: true,
+      }
+    },
+    data() {
+      return {
+        selectedOptionId: null,
+      }
+    },
+    watch: {
+      selectedOptionId() {
+        this.notifyOptionChanged();
+      }
+    },
+    methods: {
+      getOptionId(option) {
+        return `${this.id}_${option.id}`;
+      },
+      notifyOptionChanged(option) {
+        /**
+         * Change event that is emmited whenever the filter is changed.
+         * @event change
+         * @property {Object[]} new options set
+         */
+        this.$emit('change', { newValue: this.selectedOptionId });
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
