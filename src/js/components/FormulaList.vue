89040e1e72db0c945c1b873b9fdfcbc67eef69cd
QUANTIB-LEO\Leo Kolezhuk <l.kolezhuk@quantib.com>
2021-10-17 20:22:09 +0200

    Basic overflow handling for the formula list;

--- src/js/components/FormulaList.vue
+++ src/js/components/FormulaList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="table-responsive">
-    <table class="table table-striped table-hover">
+  <div class="table-responsive formula-list-table-wrapper">
+    <table class="formula-list-table table table-striped table-hover">
       <thead>
       <tr>
         <th scope="col">#</th>
@@ -15,8 +15,8 @@
       <tbody>
       <tr v-for="formula in formulas" :key="`formula${formula.id}`">
         <th scope="row">{{ formula.id }}</th>
-        <td> {{ formula.makeName }}</td>
-        <td> {{ formula.modelName }}</td>
+        <td :title="formula.makeName"> {{ formula.makeName }}</td>
+        <td :title="formula.modelName"> {{ formula.modelName }}</td>
         <td> {{ formula.yearComparisonType }} {{ formula.year }}</td>
         <td> {{ formula.fuelType }}</td>
         <td> {{ formula.risk }}</td>
@@ -57,5 +57,21 @@
 </script>
 
 <style scoped>
+  .formula-list-table-wrapper{
+    max-height: 60em;
+    overflow: auto;
+    display: inline-block;
+    width: 100%;
+  }
+  .formula-list-table > th, td{
+    max-width: 9em;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .formula-list-table > tbody > tr:nth-child(2n+1) > td, .table-striped > tbody > tr:nth-child(2n+1) > th {
+    background-color: #e8f1ff;
+  }
+
 
 </style>
