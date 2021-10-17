a7c5bcd53a202ebbf5939a0025165301f507e362
QUANTIB-LEO\Leo Kolezhuk <l.kolezhuk@quantib.com>
2021-10-17 19:21:48 +0200

    Changed layout to show/hide create formula app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
     <h1>Friss demo</h1>
-    <div class="row">
-      <div class="col-6">
-        <FormulaList ref="formulaList"></FormulaList>
+    <div class="row justify-content-md-center">
+      <div :class="{'col-8': !isCreateFormActive, 'col-5': isCreateFormActive}"
+      class="">
+        <div class="row">
+          <div :class="{'col-11': !isCreateFormActive, 'col-12': isCreateFormActive}">
+            <FormulaList ref="formulaList"></FormulaList>
+          </div>
+          <div class="col-1 px-4">
+            <button id="addFormulaButton"
+                    class="btn btn-outline-primary"
+                    v-if="!isCreateFormActive"
+                    @click="isCreateFormActive=true"
+                    title="Create formula">
+              <span class="fa fa-plus"></span>
+            </button>
+          </div>
+        </div>
       </div>
-      <div class="col-6">
-        <CreateFormula @formulaCreated="updateFormulaList"></CreateFormula>
+      <div v-if="isCreateFormActive"
+           class="col-3">
+        <CreateFormula @create="onFormulaCreated"
+                       @cancel="onFormulaCancelled">
+        </CreateFormula>
       </div>
     </div>
   </div>
@@ -20,18 +37,22 @@
     components: { FormulaList, CreateFormula },
     data() {
       return {
-
+        isCreateFormActive: false,
       }
     },
     methods: {
       updateFormulaList() {
-        console.log('Updating list');
         this.$refs.formulaList.pull();
-        // this.formulas = formulaStore.getAll();
-        console.log(`Found ${this.formulas.length} formulas.`);
+      },
+      onFormulaCreated() {
+        this.updateFormulaList();
+        this.isCreateFormActive = false;
+      },
+      onFormulaCancelled() {
+        this.isCreateFormActive = false;
       }
     },
-    created() {
+    mounted() {
       this.updateFormulaList();
     }
   }
@@ -44,5 +65,10 @@
     color: #2c3e50;
     margin-left: 5%;
     margin-right: 5%;
+  }
+
+  #addFormulaButton {
+    height: 100%;
+    width: 100%;
   }
 </style>
