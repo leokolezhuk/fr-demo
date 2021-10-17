02806daae2a1b7d505046c56f8e3f71a8019b622
QUANTIB-LEO\Leo Kolezhuk <l.kolezhuk@quantib.com>
2021-10-17 15:58:11 +0200

    Added FormulaList app

---
+++ src/js/components/FormulaList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-striped table-hover">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Make</th>
+        <th scope="col">Model</th>
+        <th scope="col">Year</th>
+        <th scope="col">Fuel</th>
+        <th scope="col">Risk</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="formula in formulas">
+        <th scope="row">{{ formula.id }}</th>
+        <td> {{ formula.makeName }}</td>
+        <td> {{ formula.modelName }}</td>
+        <td> {{ formula.yearComparisonType }} {{ formula.year }}</td>
+        <td> {{ formula.fuelType }}</td>
+        <td> {{ formula.risk }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FormulaList",
+    props: {
+      formulas: {
+        type: Array,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
